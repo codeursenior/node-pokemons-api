83ad060b931b62f2ec070043cb98fe01a193c0dd
Simon <simon.dieny@gmail.com>
2020-07-12 00:41:37 +0200

    step(add route for first pokemon)

--- app.js
+++ app.js
@@ -5,4 +5,6 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello, Express! 👋'))
 
+app.get('/api/pokemons/1', (req, res) => res.send('Hello, Bulbizarre !'))
+
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
