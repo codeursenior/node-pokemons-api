89341dd1476a8d9d20ac4b9b2ba6d57c7f75af10
Simon <simon.dieny@gmail.com>
2020-07-12 00:16:36 +0200

    step(hello, express v1)

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('Hello, Node ! 👋');
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
