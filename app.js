47dbb74f1ce3b3737238fcb1d2b416017df18340
Simon <simon.dieny@gmail.com>
2020-07-12 15:52:12 +0200

    step(add id parameter to endpoint)

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello, Express! 👋'))
 
-app.get('/api/pokemons/1', (req, res) => res.send('Hello, Bulbizarre !'))
+app.get('/api/pokemons/:id', (req, res) => {
+  const id = req.params.id
+  res.send(`Vous avez demandé le pokémon n°${id}.`)
+})
 
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
