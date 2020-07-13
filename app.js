a028f2d53996919264da6465574b2e3fe7b833b8
Simon <simon.dieny@gmail.com>
2020-07-13 14:14:14 +0200

    step(add status code for HTTP Response)

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('/api/pokemons', (req, res) => {
 app.get('/api/pokemons/:id', (req, res) => {
   const id = parseInt(req.params.id)
   const pokemon = pokemons.find(pokemon => pokemon.id === id)
-  res.json(pokemon)
+  res.status(200).json(pokemon)
 })
 
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
