e009657b99ce79b94570520e7f750910f5836e11
Simon <simon.dieny@gmail.com>
2020-07-13 10:34:19 +0200

    step(add mock pokemon and find method)

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const pokemons = require('./mock-pokemons.js');
 
 const app = express()
 const port = 3000
@@ -7,7 +8,8 @@ app.get('/', (req, res) => res.send('Hello, Express! 👋'))
 
 app.get('/api/pokemons/:id', (req, res) => {
   const id = req.params.id
-  res.send(`Vous avez demandé le pokémon n°${id}.`)
+  const pokemon = pokemons.find(pokemon => pokemon.id === id)
+  res.send(`Vous avez demandé le pokémon n°${pokemon.name}.`)
 })
 
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
