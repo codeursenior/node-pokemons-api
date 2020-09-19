a67688eec19d1378d5399a92f926564f192bc32a
Simon Dieny <simon.dieny@gmail.com>
2020-09-19 13:30:20 +0200

    fix(findPokemonByPk)

--- src/routes/findPokemonByPk.js
+++ src/routes/findPokemonByPk.js
@@ -2,8 +2,7 @@ const { Pokemon } = require('../db/sequelize')
 
 module.exports = (app) => {
   app.get('/api/pokemons/:id', (req, res) => {
-    const pokemonId = parseInt(req.params.id)
-    Pokemon.findByPk(pokemonId)
+    Pokemon.findByPk(req.params.id)
       .then(pokemon => {
         const message = 'Un pokémon a bien été trouvé.'
         res.json({ message, data: pokemon })
