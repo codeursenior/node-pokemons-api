26729c3a7b5680e9edff8c3de294e39db6a4295a
Simon Dieny <simon.dieny@gmail.com>
2020-10-02 17:17:20 +0200

    step(error-deletePokemon)

--- src/routes/deletePokemon.js
+++ src/routes/deletePokemon.js
@@ -2,15 +2,27 @@ const { Pokemon } = require('../db/sequelize')
 
 module.exports = (app) => {
   app.delete('/api/pokemons/:id', (req, res) => {
-    Pokemon.findByPk(req.params.id).then(pokemon => {
-      const pokemonDeleted = pokemon;
-      Pokemon.destroy({
-        where: { id: pokemon.id }
+    Pokemon.findByPk(req.params.id)
+      .then(pokemon => {
+        if(pokemon === null) {
+          const message = `Le pokémon demandé n'existe pas. Réessayez avec un autre identifiant.`
+          res.status(404).json({ message })
+        }
+        return pokemon
       })
-      .then(_ => {
-        const message = `Le pokémon avec l'identifiant n°${pokemonDeleted.id} a bien été supprimé.`
-        res.json({message, data: pokemonDeleted })
+      .then(pokemon => {
+        Pokemon.destroy({
+          where: { id: pokemon.id }
+        })
+        return pokemon
+      })
+      .then(pokemon => {
+        const message = `Le pokémon avec l'identifiant n°${pokemon.id} a bien été supprimé.`
+        res.json({message, data: pokemon })
+      })
+      .catch(error => {
+        const message = `Le pokémon n'a pas pu être modifié. Réessayez dans quelques instants.`
+        res.status(500).json({ message, data: error })
       })
     })
-  })
 }
