1ccf2d2860085760a03e7e3fbf547ebc22f3a6e7
Simon Dieny <simon.dieny@gmail.com>
2020-10-02 11:15:07 +0200

    step(error-createPokemon)

--- src/routes/createPokemon.js
+++ src/routes/createPokemon.js
@@ -7,5 +7,9 @@ module.exports = (app) => {
         const message = `Le pokémon ${req.body.name} a bien été crée.`
         res.json({ message, data: pokemon })
       })
+      .catch(error => {
+        const message = `Le pokémon n'a pas pu être ajouté. Réessayez dans quelques instants.`
+        res.status(500).json({ message, data: error })
+      })
   })
 }
