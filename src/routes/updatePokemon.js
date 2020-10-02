2d93eae4b7043aad294a0b1d7d119bcd2cbd0c16
Simon Dieny <simon.dieny@gmail.com>
2020-10-02 11:15:34 +0200

    step(error-updatePokemon)

--- src/routes/updatePokemon.js
+++ src/routes/updatePokemon.js
@@ -6,11 +6,20 @@ module.exports = (app) => {
     Pokemon.update(req.body, {
       where: { id: id }
     })
-    .then(_ => {
+    .then(results => {
+      if(results[0] === 0) {
+        const message = `Le pokémon demandé n'existe pas. Réessayez avec un autre identifiant.`
+        res.status(404).json({ message })
+      }
+
       Pokemon.findByPk(id).then(pokemon => {
         const message = `Le pokémon ${pokemon.name} a bien été modifié.`
         res.json({message, data: pokemon })
       })
+    })
+    .catch(error => {
+      const message = `Le pokémon n'a pas pu être modifié. Réessayez dans quelques instants.`
+      res.status(500).json({ message, data: error })
     })
   })
 }
