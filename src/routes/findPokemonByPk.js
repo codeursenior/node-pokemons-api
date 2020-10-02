ce248f7b07f1e1f7aedf387acb0f6181fd5000d6
Simon Dieny <simon.dieny@gmail.com>
2020-10-02 11:14:47 +0200

    step(error-findPokemonByPk)

--- src/routes/findPokemonByPk.js
+++ src/routes/findPokemonByPk.js
@@ -4,8 +4,17 @@ module.exports = (app) => {
   app.get('/api/pokemons/:id', (req, res) => {
     Pokemon.findByPk(req.params.id)
       .then(pokemon => {
+        if(pokemon === null) {
+          const message = `Le pokémon demandé n'existe pas. Réessayez avec un autre identifiant.`
+          res.status(404).json({ message })
+        }
+
         const message = 'Un pokémon a bien été trouvé.'
         res.json({ message, data: pokemon })
+      })
+      .catch(error => {
+        const message = `Le pokémon n'a pas pu être récupéré. Réessayez dans quelques instants.`
+        res.status(500).json({ message, data: error })
       })
   })
 }
