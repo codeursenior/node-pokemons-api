12db8eb7e7486e1343546449da331fbd95c7edf6
Simon Dieny <simon.dieny@gmail.com>
2020-10-02 11:13:16 +0200

    step(error-findAllPokemons)

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -4,8 +4,13 @@ module.exports = (app) => {
   app.get('/api/pokemons', (req, res) => {
     Pokemon.findAll()
       .then(pokemons => {
-        const message = 'La liste des pokémons a bien été récupérée.'
+        const message = 'La liste des pokémons a bien été récupéré.'
         res.json({ message, data: pokemons })
+      })
+      .catch(error => {
+        const message = `La liste des pokémons n'a pas pu être récupéré. 
+                         Réessayez dans quelques instants.`
+        res.status(500).json({ message, data: error })
       })
   })
 }
