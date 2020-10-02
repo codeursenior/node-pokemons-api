aef8394553f29285d51f6a6f20c1495a0760d5da
Simon Dieny <simon.dieny@gmail.com>
2020-10-02 11:51:40 +0200

    next(error-updatePokemon)

--- src/routes/updatePokemon.js
+++ src/routes/updatePokemon.js
@@ -3,19 +3,17 @@ const { Pokemon } = require('../db/sequelize')
 module.exports = (app) => {
   app.put('/api/pokemons/:id', (req, res) => {
     const id = req.params.id
-    Pokemon.update(req.body, {
-      where: { id: id }
-    })
+    Pokemon.update(req.body, { where: { id: id } })
     .then(results => {
       if(results[0] === 0) {
         const message = `Le pokémon demandé n'existe pas. Réessayez avec un autre identifiant.`
         res.status(404).json({ message })
       }
-
-      Pokemon.findByPk(id).then(pokemon => {
-        const message = `Le pokémon ${pokemon.name} a bien été modifié.`
-        res.json({message, data: pokemon })
-      })
+    })
+    .then(_ => Pokemon.findByPk(id))
+    .then(pokemon => {
+      const message = `Le pokémon ${pokemon.name} a bien été modifié.`
+      res.json({message, data: pokemon })
     })
     .catch(error => {
       const message = `Le pokémon n'a pas pu être modifié. Réessayez dans quelques instants.`
