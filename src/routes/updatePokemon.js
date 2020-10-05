f7a8759bf480901da1749e62fe81e55d3f6da029
Simon Dieny <simon.dieny@gmail.com>
2020-10-05 10:34:48 +0200

    next(handle-ValidationError)

--- src/routes/updatePokemon.js
+++ src/routes/updatePokemon.js
@@ -1,4 +1,5 @@
 const { Pokemon } = require('../db/sequelize')
+const { ValidationError } = require('sequelize')
 
 module.exports = (app) => {
   app.put('/api/pokemons/:id', (req, res) => {
@@ -18,6 +19,9 @@ module.exports = (app) => {
       })
     })
     .catch(error => {
+      if(error instanceof ValidationError) {
+        return res.status(400).json({ message: error.message, data: error });
+      }
       const message = `Le pokémon n'a pas pu être modifié. Réessayez dans quelques instants.`
       res.status(500).json({ message, data: error })
     })
