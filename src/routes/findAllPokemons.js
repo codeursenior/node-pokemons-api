ce52a4fe87bc28537f2b480b0cd83fc54ec1b2c1
Simon Dieny <simon.dieny@gmail.com>
2020-10-13 09:57:29 +0200

    step(setup-exact-search-with-operator)

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,10 +1,17 @@
 const { Pokemon } = require('../db/sequelize')
+const { Op } = require('sequelize')
 
 module.exports = (app) => {
   app.get('/api/pokemons', (req, res) => {
     if(req.query.name) {
       const name = req.query.name
-      return Pokemon.findAll({ where: { name: name } })
+      return Pokemon.findAll({ 
+        where: { 
+          name: {
+            [Op.eq]: name
+          }
+        } 
+      })
       .then(pokemons => {
         const message = `Il y a ${pokemons.length} qui correspondent au terme de recherche ${name}.`
         return res.json({ message, data: pokemons })
