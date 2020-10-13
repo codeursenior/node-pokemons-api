fadef4b343ec04b2e9e58fcb4ea636eba76bb701
Simon Dieny <simon.dieny@gmail.com>
2020-10-13 10:10:47 +0200

    step(search-with-operator-like)

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -8,7 +8,7 @@ module.exports = (app) => {
       return Pokemon.findAll({ 
         where: { 
           name: {
-            [Op.eq]: name
+            [Op.like]: `%${name}%`
           }
         } 
       })
