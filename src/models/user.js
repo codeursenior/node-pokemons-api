2fc0896fec94b1b4fbb473e5482b02d0a4400ead
Simon Dieny <simon.dieny@gmail.com>
2020-10-29 10:15:22 +0100

    step(validator-user-model)

--- src/models/user.js
+++ src/models/user.js
@@ -6,10 +6,19 @@ module.exports = (sequelize, DataTypes) => {
       autoIncrement: true
     },
     username: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      unique: {
+        msg: 'Le nom est déjà pris.'
+      }
     },
     password: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [6, 25],
+          msg: 'Le mot de passe doit contenir entre 6 et 25 caractères.'
+        }
+      }
     }
   })
 }
