90eab511352bd129cdbf40fbaf18f6ca632aef28
Simon Dieny <simon.dieny@gmail.com>
2020-10-29 11:53:08 +0100

    step(handle-error-login)

--- src/routes/login.js
+++ src/routes/login.js
@@ -5,12 +5,25 @@ module.exports = (app) => {
   app.post('/api/login', (req, res) => {
 
     User.findOne({ where: { username: req.body.username } }).then(user => {
-      bcrypt.compare(req.body.password, user.password).then(isPasswordValid => {
-        if(isPasswordValid) {
-          const message = `L'utilisateur a été connecté avec succès`;
-          return res.json({ message, data: user })
+
+      if(!user) {
+        const message = `L'utilisateur demandé n'existe pas.`
+        return res.status(401).json({ message })
+      }
+
+      return bcrypt.compare(req.body.password, user.password).then(isPasswordValid => {
+        if(!isPasswordValid) {
+          const message = `Le mot de passe est incorrect.`
+          return res.status(401).json({message})
         }
+
+        const message = `L'utilisateur a été connecté avec succès`;
+        return res.json({ message, data: user })
       })
+    })
+    .catch(error => {
+      const message = `L'utilisateur n'a pas pu être connecté. Réessayez dans quelques instants.`
+      res.status(500).json({ message, data: error })
     })
   })
 }
