61be8734bb91b10d643dd0c1eff94b159ae7a183
Simon Dieny <simon.dieny@gmail.com>
2020-10-30 09:04:49 +0100

    step(add-auth-middleware)

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,25 +1,27 @@
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken')
 const privateKey = require('../auth/private_key')
 
 module.exports = (req, res, next) => {
+  const authorizationHeader = req.headers.authorization
 
-  const token = req.headers.authorization.split(' ')[1];
-  const decodedToken = jwt.verify(token, privateKey);
-
-  // TODO : créer le middleware d'authentification 401 !
-
-  try {
-    const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, privateKey);
-    const userId = decodedToken.userId;
-    if (req.body.userId && req.body.userId !== userId) {
-      throw 'Invalid user ID';
-    } else {
-      next();
-    }
-  } catch {
-    res.status(401).json({
-      error: new Error('Invalid request!')
-    });
+  if(!authorizationHeader) {
+    const message = `Vous n'avez pas fourni de jeton d'authentification. Ajoutez-en un dans l'en-tête de la requête.`
+    return res.status(401).json({ message })
   }
-};
+  
+  const token = authorizationHeader.split(' ')[1]
+  const decodedToken = jwt.verify(token, privateKey, (error, decodedToken) => {
+    if(error) {
+      const message = `L'utilisateur n'est pas autorisé à accèder à cette ressource.`
+      return res.status(401).json({ message, data: error })
+    }
+
+    const userId = decodedToken.userId
+    if (req.body.userId && req.body.userId !== userId) {
+      const message = `L'identifiant de l'utilisateur est invalide.`
+      res.status(401).json({ message })
+    } else {
+      next()
+    }
+  })
+}
