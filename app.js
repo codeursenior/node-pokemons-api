c1d8f01bd5be242e38412577fa1b984b3e63bc44
Simon Dieny <simon.dieny@gmail.com>
2020-11-13 22:37:38 +0100

    remove morgan from project

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const express = require('express')
-// const morgan = require('morgan')
 const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
 const sequelize = require('./src/db/sequelize')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app
 .use(favicon(__dirname + '/favicon.ico'))
-// .use(morgan('dev'))
 .use(bodyParser.json())
 
 sequelize.initDb()
