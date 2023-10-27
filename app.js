5ea2e1d498a839eb27e2707cbde071be655008c8
senopamungkas <147467325+senopamungkas@users.noreply.github.com>
2023-10-27 09:38:40 +0800

    initial commit

--- app.js
+++ app.js
@@ -1,34 +1,41 @@
-const express = require('express')
-const favicon = require('serve-favicon')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const sequelize = require('./src/db/sequelize')
+const express = require("express");
+const favicon = require("serve-favicon");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const sequelize = require("./src/db/sequelize");
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 app
-.use(favicon(__dirname + '/favicon.ico'))
-.use(bodyParser.json())
-.use(cors())
+  .use(favicon(__dirname + "/favicon.ico"))
+  .use(bodyParser.json())
+  .use(cors());
 
-sequelize.initDb()
+sequelize.initDb();
 
-app.get('/', (req, res) => {
-  res.json('Hello, Heroku ! 👋')
-})
-
-require('./src/routes/findAllPokemons')(app)
-require('./src/routes/findPokemonByPk')(app)
-require('./src/routes/createPokemon')(app)
-require('./src/routes/updatePokemon')(app)
-require('./src/routes/deletePokemon')(app)
-require('./src/routes/login')(app)
+app.get("/", (req, res) => {
+  res.json("Hello, Heroku ! 👋");
+});
+app.get("/tes", (req, res) => {
+  res.json("teesss Hello, Heroku ! 👋");
+});
+require("./src/routes/findAllPokemons")(app);
+require("./src/routes/findPokemonByPk")(app);
+require("./src/routes/createPokemon")(app);
+require("./src/routes/updatePokemon")(app);
+require("./src/routes/deletePokemon")(app);
+require("./src/routes/login")(app);
 
 // On gère les routes 404.
-app.use(({res}) => {
-  const message = 'Impossible de trouver la ressource demandée ! Vous pouvez essayer une autre URL.'
-	res.status(404).json({message});
+app.use(({ res }) => {
+  const message =
+    "Impossible de trouver la ressource demandée ! Vous pouvez essayer une autre URL.";
+  res.status(404).json({ message });
 });
 
-app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
+app.listen(port, () =>
+  console.log(
+    `Notre application Node est démarrée sur : http://localhost:${port}`
+  )
+);
